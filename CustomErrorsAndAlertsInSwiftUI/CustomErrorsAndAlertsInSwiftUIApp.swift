c302f88455c7f25715bd8a78e37abd0a29537e91
ram4ik <ram4ik@hotmail.com>
2023-10-22 10:21:50 +0300

    Initial Commit

---
+++ CustomErrorsAndAlertsInSwiftUI/CustomErrorsAndAlertsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomErrorsAndAlertsInSwiftUIApp.swift
+//  CustomErrorsAndAlertsInSwiftUI
+//
+//  Created by admin on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomErrorsAndAlertsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
