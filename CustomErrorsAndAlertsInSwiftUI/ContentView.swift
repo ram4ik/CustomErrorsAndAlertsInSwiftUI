a5b7b545a61e7a0c64fe6cc5af27537a1693844c
ram4ik <ram4ik@hotmail.com>
2023-10-22 10:32:12 +0300

    init alert

--- CustomErrorsAndAlertsInSwiftUI/ContentView.swift
+++ CustomErrorsAndAlertsInSwiftUI/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+extension Binding where Value == Bool {
+    
+    init<T>(value: Binding<T?>) {
+        self.init {
+            value.wrappedValue != nil
+        } set: { newValue in
+            if !newValue {
+                value.wrappedValue = nil
+            }
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var errorTitle: String? = nil
+    
+    var body: some View {
+        Button("Click Me") {
+            saveData()
+        }
+        .alert(errorTitle ?? "Error", isPresented: Binding(value: $errorTitle)) {
+            Button("Ok") {
+                
+            }
+        }
+    }
+    
+    private func saveData() {
+        let isSuccessful: Bool = false
+        
+        if isSuccessful {
+            
+        } else {
+            errorTitle = "An error occured!"
+        }
     }
 }
 
